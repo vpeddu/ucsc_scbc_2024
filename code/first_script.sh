f33d22e532f70e087ade147a21dce88b56ba0b55
rreggiar <rreggiar@ucsc.edu>
2022-07-18 16:12:09 -0700

    adding 01c

--- code/first_script.sh
+++ code/first_script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # rreggiar@ucsc.edu
-# 2022-07-10
+# 2022-07-18
 
 script_path=$0
 script_name=$(basename $0)
 input_var=${1:-10}
+input_user=$USER
 
 echo The name of this script is: $script_name
 
-echo $input_var%%2
+echo The user of this script is: $input_user
+
+echo $input_var / 2 = $((input_var/2))
 
