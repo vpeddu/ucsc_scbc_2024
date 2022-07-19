b6b6d9a146c934d0da7508026778d64009d6d83a
rreggiar <rreggiar@ucsc.edu>
2022-07-18 23:38:12 -0700

    update second script and slides with IF statement, etc

--- code/second_script.sh
+++ code/second_script.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
+# second_script.sh
 # rreggiar@ucsc.edu
 # 2022-07-18
 
-script_path=$0 # $0 stores the name of the script in its path
+cmd_recieved=$0 # $0 stores the command entered to the cmd line
 script_name=$(basename $0) # basename extracts the last entry in a path
 input_var=${1:-10} # $1 stores the first cmd line argument
-input_userID=$UID # $USER is a global bash variable that stores your user ID
+# :-VAL sets VAL to the default value of the argument
+print_info=${2:-"TRUE"} # $2 stores the second cmd line argument
 
-echo The name of this script is: $script_name 
-# echo can print combinations of text and variables
-echo Your user ID is: $input_userID
-# bash can do math inside $(())
-echo $input_var / 2 = $((input_var/2))
+if [ $print_info = "TRUE" ]; then
+	echo "command: " $cmd_recieved
+	# echo can print combinations of text and variables
+	echo "The name of this script is: " $script_name 
+	# bash can do math inside $(())
+	echo $input_var / 2 = $((input_var/2))
+fi
 
